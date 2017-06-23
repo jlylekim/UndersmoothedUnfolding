8d9efb247ed7f5b20ac42f35272908db50418a8f
Mikael Kuusela <mikael.kuusela@gmail.com>
2017-06-22 23:49:28 -0400

    Changes to the intro in UndersmoothTauSimulation.cxx

--- UndersmoothTauSimulation.cxx
+++ UndersmoothTauSimulation.cxx
@@ -1,6 +1,24 @@
 //==============================================================================
-//  Junhyung (Lyle) Kim
+//  Authors: Junhyung (Lyle) Kim and Mikael Kuusela
 //  June 20, 2017
+//
+//  This code demonstrates how to use the UndersmoothTau function and performs
+//  a coverage study comparing intervals from UndersmoothTau and ScanLcurve
+//
+//------------------------------------------------------------------------------
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //==============================================================================
 
